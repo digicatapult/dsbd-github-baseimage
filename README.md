7fa2aa04ceca7b78f77d9fc4c70e48ae4ccb9f46
David Blane <david.blane@digicatapult.org.uk>
2024-02-13 08:41:14 +0000

    Updated scripts to allow passthrough of Github PAT token from AWS/Azure

--- README.md
+++ README.md
@@ -19,6 +19,22 @@ az sig image-definition create --resource-group dsbd-github-images --gallery-nam
  --sku 22_04-lts-arm64 --os-type linux --hyper-v-generation V2 --architecture Arm64
 ```
 
-You will need to provide some vars, namely, the resource group and the subscription id which have been purposefully omitted from the repo as they are considered secret, these can be loaded automatically from a file called `secrets.auto.pkrvars.hcl` in the root of the repo.
+You will need to provide some vars, namely, the resource group and the subscription id which have been purposefully omitted from the repo as they are considered secret, these can be loaded automatically from a file called `secrets.auto.pkrvars.hcl` in the root of the repo.  You will also need to supply the AWS credentials in the same file, e.g. aws_access_key and aws_secret_key.
 
 Run using `packer build .` in the root of the repo.
+
+To only run one of the sources e.g. the aws source, use `packer build -only=amazon-ebs.ubuntu .`
+
+### Retrieving Github Actions PAT from Azure Key Vault or AWS Secrets Manager
+
+The PAT is stored in Azure Key Vault or AWS Secrets Manager.  The PAT is used to authenticate to Github and retrieve a token for registering a self served Github Actions runner.  Using cloudInit please provide the following ENVARS for the `update_qemu_morello_config.sh` script to retrieve the PAT from the respective secret store.  We suggest using cloudinit to write the environment variables to `/etc/environment` so that they are available to the `update_qemu_morello_config.sh` script.
+
+```yaml
+write_files:
+- path: /etc/environment
+  content: |
+    SECRET_SOURCE=azure # or aws
+    SECRET=github-actions # or the name of the secret in the respective secret store
+    KEY_VAULT_NAME=dsbd-github-images # the name of the key vault, only required for Azure
+  append: true
+```
--- files/update_qemu_morello_config.sh
+++ files/update_qemu_morello_config.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-# Configurable Variables
-CONFIG_FILE="/etc/sysconfig/qemu-morello.conf"
-BUFFER_MEM_MB=1024  # Memory buffer in MB reserved for the system
-MIN_CPU_THREADS=1     # Minimum number of CPU threads for QEMU
-CPU_DEDUCTION=2     # Number of CPUs to reserve for the host
+# Configurable Variables with environment variable overrides or defaults
+CONFIG_FILE="${CONFIG_FILE:-/etc/sysconfig/qemu-morello.conf}"
+BUFFER_MEM_MB="${BUFFER_MEM_MB:-1024}"  # Memory buffer in MB reserved for the system, default 1024 MB
+MIN_CPU_THREADS="${MIN_CPU_THREADS:-1}"   # Minimum number of CPU threads for QEMU, default 1
+CPU_DEDUCTION="${CPU_DEDUCTION:-2}"     # Number of CPUs to reserve for the host, default 2
+KEY_VAULT_NAME="${KEY_VAULT_NAME:-YourKeyVaultName}"  # Default Key Vault name for Azure
+SECRET_NAME="${SECRET_NAME:-GitHubPAT}"               # Default secret name for both Azure and AWS
+SECRET_SOURCE="${SECRET_SOURCE:-azure}"  # Source of the secret: 'azure' or 'aws'
 
 # Logging function
 log() {
@@ -15,6 +18,19 @@ log() {
 handle_error() {
     log "Error: $1"
     exit 1
+}
+
+# Function to check if CLI tools are installed
+check_cli_tools() {
+    if [ "$SECRET_SOURCE" == "azure" ]; then
+        if ! command -v az &> /dev/null; then
+            handle_error "Azure CLI (az) could not be found. Please install it to proceed."
+        fi
+    elif [ "$SECRET_SOURCE" == "aws" ]; then
+        if ! command -v aws &> /dev/null; then
+            handle_error "AWS CLI (aws) could not be found. Please install it to proceed."
+        fi
+    fi
 }
 
 # Function to get the total number of CPU sockets
@@ -48,6 +64,35 @@ write_config() {
     log "QEMU Morello service configuration updated successfully."
 }
 
+# Function to fetch GitHub PAT from Azure Key Vault or AWS Secrets Manager
+fetch_github_pat() {
+    check_cli_tools  # Ensure CLI tools are available
+
+    local pat=""
+    if [ "$SECRET_SOURCE" == "azure" ]; then
+        pat=$(az keyvault secret show --name "$SECRET_NAME" --vault-name "$KEY_VAULT_NAME" --query value -o tsv)
+    elif [ "$SECRET_SOURCE" == "aws" ]; then
+        pat=$(aws secretsmanager get-secret-value --secret-id "$SECRET_NAME" --query 'SecretString' --output text)
+    else
+        handle_error "Invalid SECRET_SOURCE specified. Must be 'azure' or 'aws'."
+    fi
+
+    if [[ -z "$pat" ]]; then
+        handle_error "Failed to fetch GitHub PAT from $SECRET_SOURCE."
+    else
+        log "GitHub PAT fetched successfully from $SECRET_SOURCE."
+    fi
+
+    echo "$pat"
+}
+
 # Main execution
 log "Updating QEMU Morello service configuration..."
 write_config
+# Fetch GitHub PAT and write to a secure location in the smbshare subdirectory
+mkdir -p /etc/qemu-morello/smbshare
+PAT=$(fetch_github_pat)
+echo "$PAT" > /etc/qemu-morello/smbshare/github_pat.secret
+chmod 600 /etc/qemu-morello/smbshare/github_pat.secret
+chown -R cheri:cheri /etc/qemu-morello/smbshare
+log "GitHub PAT stored securely in smbshare directory."
